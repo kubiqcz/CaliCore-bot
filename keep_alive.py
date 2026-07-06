e03c15fcf69eb5a0a6a4c60cb22de1df5ed97454
kubiqcz <141441649+kubiqcz@users.noreply.github.com>
2026-07-06 14:40:08 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,11 +5,13 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "CaliCore běží a je online!"
+    return "CaliCore MDT Server je online a čeká na příkazy!"
 
 def run():
+    # Spustí webový server na portu, který Render vyžaduje
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
+    # Přesune webový server do vedlejšího vlákna, aby neblokoval Discord bota
     t = Thread(target=run)
     t.start()
